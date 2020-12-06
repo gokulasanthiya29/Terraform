22e3fa2f3f1f23b534dda811d25c13d28acec411
Gokula Santhiya R <58739600+gokulasanthiya29@users.noreply.github.com>
2020-12-06 21:11:00 +0530

    Add files via upload
    Modified tags

---
+++ Modification.with.terraform.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+    region="us-east-1"
+    access_key = "AKIAIPGAZ6V57PYNNYTA"
+    secret_key = "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"
+}
+
+resource "aws_instance" "terraform-provisioned-EC2" {
+  ami           = "ami-04d29b6f966df1537"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF EC2"
+  }
+}
