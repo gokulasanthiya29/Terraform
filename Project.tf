dfd0088702d0dbd80d7af6120c76efa9c59b099c
Gokula Santhiya R <santhiyasandyrc@gmail.com>
2020-12-08 22:43:00 +0530

    Update Project.tf

--- Project.tf
+++ Project.tf
@@ -68,12 +68,20 @@ resource "aws_security_group" "tf-project-sg" {
   }
   ingress {
     description = "HTTP"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+    
+  ingress {
+    description = "SSH"
+    from_part = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+      
   egress {
     from_port   = 0
     to_port     = 0
